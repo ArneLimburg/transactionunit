5e166941b8685c9c315c6fd8e4d6608e24b52e0c
Olaf Prins <olaf.prins@openknowledge.de>
2021-10-09 11:53:28 +0200

    Add integration test for Spring Boot

--- src/test/java/io/github/arnelimburg/transactionunit/spring/IntegrationTest.java
+++ src/test/java/io/github/arnelimburg/transactionunit/spring/IntegrationTest.java
@@ -15,11 +15,52 @@
  */
 package io.github.arnelimburg.transactionunit.spring;
 
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * @author Olaf Prins - open knowledge GmbH
  */
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 @SpringBootTest
 public class IntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Test
+    void createAndReadUser() throws Exception {
+        // Create user
+
+        String jsonBody =
+            "{\n"
+            + "    \"id\": 1,\n"
+            + "    \"name\": \"Joe Doe\"\n"
+            + "}";
+
+        mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody))
+            .andExpect(status().isCreated());
+
+        // List users
+
+        mockMvc.perform(get("/users"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(1)))
+            .andExpect(jsonPath("$.[0].name", is("Joe Doe")));
+    }
 }
